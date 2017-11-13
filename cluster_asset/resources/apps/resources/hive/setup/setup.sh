c36a918022bd0e127ac233e62a810e4c232715a4
sreddy <sreddy@dnanexus.com>
2017-11-13 10:09:44 -0800

    Adding sudo to create ln

--- cluster_asset/resources/apps/resources/hive/setup/setup.sh
+++ cluster_asset/resources/apps/resources/hive/setup/setup.sh
@@ -4,4 +4,4 @@ set -e -x -o pipefail
 echo "Setting hive environment"
 source /apps/resources/hive/hive.environment
 
-ln -s /usr/share/java/mysql-connector-java.jar /apps/hive/lib/mysql-connnector-java.jar
+sudo ln -s /usr/share/java/mysql-connector-java.jar /apps/hive/lib/mysql-connnector-java.jar
