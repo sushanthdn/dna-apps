0f2f83cb9fcf5d3d03d0093ad95eb13f0205983c
sreddy <sreddy@dnanexus.com>
2019-01-04 10:59:11 -0800

    Move it to the right folder

--- cluster-apps/pitest/src/pitest.sh
+++ cluster-apps/pitest/src/pitest.sh
@@ -7,11 +7,15 @@ main() {
     echo "Value of no_of_samples: '$no_of_samples'"
 
     /cluster/hadoop/bin/hdfs dfsadmin -report
-    logPropFile=/cluster/dnax/config/log/log4j-INFO.properties
 
     #sleep 50000
 
-    python /cluster/dnax/bin/dx-spark-submit.py --log-level INFO --collect-log --app-config /scripts/app.json --user-config /scripts/user.json \
-        --spark-args '--class org.apache.spark.examples.SparkPi /cluster/spark/examples/jars/spark-examples*.jar 10'
+    python /cluster/dnax/bin/dx-spark-submit.py \
+        --log-level INFO \
+        --collect-log \
+        ----log-collect-dir pitestlogs \
+        --app-config /scripts/app.json \
+        --user-config /scripts/user.json \
+        --spark-args "--class org.apache.spark.examples.SparkPi /cluster/spark/examples/jars/spark-examples*.jar $no_of_samples"
 
 }
