41d238e2497710872d62dda95395558634a83b23
sreddy <sreddy@dnanexus.com>
2019-02-15 10:17:43 -0800

    Subjob-cluster app

---
+++ cluster-apps/subjob-cluster/src/subjob-cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# subjob-cluster 0.0.1
+
+pitest()
+{
+
+    /cluster/hadoop/bin/hdfs dfsadmin -report
+
+    python /cluster/dnax/bin/dx-spark-submit.py \
+        --spark-args "--class org.apache.spark.examples.SparkPi /cluster/spark/examples/jars/spark-examples*.jar 10"
+
+}
+
+main()
+{
+
+    job = dx-jobutil-new-job pitest
+    echo " MAIN JOB DONE "
+}
